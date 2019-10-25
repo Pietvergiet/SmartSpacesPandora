8c8091bcc498f455d2b2a8b7a0ee0fef69d2f6e9
Jelle Maas <jelle.j.maas@gmail.com>
2019-10-25 21:40:10 +0200

    WekaClassifiers + de Wrappers, Hierna zijn in ExplorerGamescreen de activities en lockpicking, hier moet waar de foutmelding is nog de location.object is lock worden toegevoegd, geen idee hoe dat moet want dat is er nog niet, maar dit is alles! weka.jar moet misschien wel door iedereen apart worden toegevoegd maar idk, succes en fijn weekend joejoe

--- app/src/main/java/smartSpaces/Pandora/ExplorerGameScreen.java
+++ app/src/main/java/smartSpaces/Pandora/ExplorerGameScreen.java
@@ -12,12 +12,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Typeface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.text.InputFilter;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -39,10 +46,14 @@ import org.w3c.dom.Text;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import pl.droidsonroids.gif.GifImageView;
+import smartSpaces.Pandora.Game.Activities.WekaClassifierActivities;
+import smartSpaces.Pandora.Game.Activities.WekaClassifierPicklock;
 import smartSpaces.Pandora.Game.GameClient;
 import smartSpaces.Pandora.Game.Panel;
 import smartSpaces.Pandora.Game.Player;
@@ -51,7 +62,7 @@ import smartSpaces.Pandora.P2P.BluetoothServiceFragment;
 import smartSpaces.Pandora.P2P.Constants;
 import smartSpaces.Pandora.Picklock.R;
 
-public class ExplorerGameScreen extends AppCompatActivity {
+public class ExplorerGameScreen extends AppCompatActivity implements SensorEventListener {
 
     // controller stuff
     private GameClient game;
@@ -78,10 +89,59 @@ public class ExplorerGameScreen extends AppCompatActivity {
     public String coords = "";
     NFCReader reader = new NFCReader();
 
+
+    //Activity Recognition meuk
+    int stableamount = 9;
+    double maxValue;
+    double maxCount;
+
+    private boolean accChanged, gyroChanged, magnetoChanged;
+
+    List<Double> activitystable = new ArrayList<>();
+
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer, gyroscope;
+
+    private double accXValue, accYValue, accZValue;
+    private double gyroXvalue, gyroYValue, gyroZValue;
+
+
+    //ALL VARIABLES FOR PICKLOCK
+    int elapsedTime;
+    long startTime;
+
+    int flatTime = 0;
+    int rightTime = 0;
+    int middleTime = 0;
+    int downTime = 0;
+    int leftTime = 0;
+
+    int correctTime = 5;
+
+    String activity;
+    List picklock = new ArrayList();
+
+    List<Double> picklockstable = new ArrayList<>();
+
+    boolean lockpickbool = false;
+    boolean done = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.explorer_wait_screen);
+
+        //SENSORS
+        //magnetometer could be uncommented if it wants to be used
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        //magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensorManager.registerListener(this, accelerometer, sensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroscope, sensorManager.SENSOR_DELAY_NORMAL);
+        //sensorManager.registerListener(this, magnetometer, sensorManager.SENSOR_DELAY_NORMAL);
 
         //NFC stuff
         context = this;
@@ -439,5 +499,290 @@ public class ExplorerGameScreen extends AppCompatActivity {
     public void onResume(){
         super.onResume();
         nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //Dit is hoe het geimplement zou moeten worden, maar daar hebben we dus location enzo voor nodig.
+        if(location.object == lock && done ==false){
+            if(lockpickbool == false){
+                lockpickbool = true;
+                Lockpicker();
+            }
+            checkSensorsChanged(event);
+            if (accChanged && gyroChanged ) {
+
+                Double[] arraypicklock = getSensorData();
+
+                try{
+                    double resultpick = WekaClassifierPicklock.classify(arraypicklock);
+                    if(picklockstable.size() == 0){
+                        startTime = System.nanoTime();
+                    }
+                    picklockstable.add(resultpick);
+                    if(picklockstable.size() == stableamount){
+
+                        String textresultpicklock;
+                        elapsedTime = (int)((System.nanoTime() - startTime)/1000000);
+                        resultpick = getBestClassifiedActivity();
+
+                        if(resultpick == 0.0){
+
+                            textresultpicklock = "Left";
+                            flatTime = 0;
+                            rightTime = 0;
+                            middleTime = 0;
+                            downTime = 0;
+                            leftTime= leftTime + elapsedTime;
+                            picklockstable.clear();
+                            if(activity.equals(textresultpicklock)){
+                                Lockpickerresult(leftTime);
+                            }
+                        }else if(resultpick == 1.0) {
+                            textresultpicklock = "Down";
+                            flatTime = 0;
+                            rightTime = 0;
+                            middleTime = 0;
+                            leftTime = 0;
+                            downTime= downTime + elapsedTime;
+                            picklockstable.clear();
+                            if(activity.equals(textresultpicklock)){
+                                Lockpickerresult(downTime);
+                            }
+                        }else if(resultpick == 2.0) {
+                            textresultpicklock = "Flat";
+                            rightTime = 0;
+                            middleTime = 0;
+                            leftTime = 0;
+                            downTime = 0;
+
+                            flatTime= flatTime + elapsedTime;
+                            picklockstable.clear();
+                            if(activity.equals(textresultpicklock)){
+                                Lockpickerresult(flatTime);
+                            }
+                        }else if(resultpick == 3.0) {
+                            textresultpicklock = "Right";
+                            middleTime = 0;
+                            leftTime = 0;
+                            downTime = 0;
+                            flatTime = 0;
+
+
+                            rightTime= rightTime + elapsedTime;
+                            picklockstable.clear();
+                            if(activity.equals(textresultpicklock)){
+                                Lockpickerresult(rightTime);
+                            }
+                        }else if(resultpick == 4.0) {
+                            rightTime = 0;
+                            leftTime = 0;
+                            downTime = 0;
+                            flatTime = 0;
+                            textresultpicklock = "Middle";
+                            middleTime= middleTime + elapsedTime;
+                            picklockstable.clear();
+                            if(activity.equals(textresultpicklock)){
+                                Lockpickerresult(middleTime);
+                            }
+                        }else{
+                            textresultpicklock = "you are doing nothing";
+                            picklockstable.clear();
+                        }
+
+                        System.out.println(textresultpicklock);
+                        System.out.println("volgende take zijn: " + picklock);
+
+                    }
+
+
+                }catch (Exception e){
+                    System.out.println("and it stopped working");
+                }
+            }
+
+
+        }
+
+        checkSensorsChanged(event);
+        changedSensorActivity();
+    }
+
+    private void changedSensorActivity() {
+        if (accChanged && gyroChanged ){
+            Double[] array = getSensorData();
+
+            try{
+                classifyData(array);
+            }catch (Exception e){
+                System.out.println("and it stopped working");
+            }
+        }
+    }
+
+    private void classifyData(Double[] array) throws Exception {
+        double result = WekaClassifierActivities.classify(array);
+
+        String textresult;
+
+        activitystable.add(result);
+        if(activitystable.size() == stableamount){
+
+
+            result = getBestClassifiedActivity();
+
+            if(result == 0.0){
+
+                textresult = "you are Flag";
+
+                activitystable.clear();
+            }else if(result == 1.0) {
+                textresult = "you are Still";
+
+                activitystable.clear();
+            }else if(result == 2.0) {
+                textresult = "you are Shaking";
+
+                activitystable.clear();
+            }else if(result == 3.0) {
+                textresult = "you are pirhouette";
+
+                activitystable.clear();
+            }else{
+                textresult = "you are doing nothing";
+
+                activitystable.clear();
+            }
+
+            //TextView placeText = (TextView)findViewById(R.id.textresult);
+            //placeText.setText(textresult);
+
+        }
+    }
+
+    private double getBestClassifiedActivity() {
+        double result;
+        for (int i = 0; i < activitystable.size(); ++i) {
+            int count = 0;
+            for (int j = 0; j < activitystable.size(); ++j) {
+                if (activitystable.get(j) == activitystable.get(i)) ++count;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                maxValue = activitystable.get(i);
+            }
+        }
+
+        result = maxValue;
+        maxValue = -1;
+        maxCount = -1;
+        return result;
+    }
+
+    private Double[] getSensorData() {
+        accChanged = false;
+        gyroChanged = false;
+        Double array [] = new Double[6];
+        array[0] = accXValue;
+        array[1] = accYValue;
+        array[2] = accZValue;
+        array[3] = gyroXvalue;
+        array[4] = gyroYValue;
+        array[5] = gyroZValue;
+
+        return array;
+    }
+
+    private void checkSensorsChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+
+        Log.d(TAG, "onSensorChanged: sensor: " + Sensor.TYPE_ACCELEROMETER);
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+
+            this.accXValue = event.values[0];
+            this.accYValue = event.values[1];
+            this.accZValue = event.values[2];
+            accChanged = true;
+            Log.d(TAG, "startTracking: sensordata changed: x: " + this.accXValue + ", y: " + this.accYValue + ", z: " + this.accZValue);
+        } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            this.gyroXvalue = event.values[0];
+            this.gyroYValue = event.values[1];
+            this.gyroZValue = event.values[2];
+            gyroChanged = true;
+        }
+
+    }
+
+
+    public void Vibrate(){
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
+    }
+
+    public void Lockpicker(){
+        picklock = randomActions(randomNumber());
+        System.out.println("allemaal dingen" + picklock);
+        activity = picklock.get(0).toString();
+        //String textresult = activity;
+        //TextView placeText = (TextView)findViewById(R.id.textresult);
+        //placeText.setText(textresult);
+    }
+    public void Lockpickerresult(int time){
+
+        if(correctTime < time){
+            picklock.remove(0);
+            Vibrate();
+            if(picklock.size() != 0){
+                activity = picklock.get(0).toString();
+
+            }else{
+                activity = "you did lockpicked it!";
+                lockpickbool = false;
+                done = true;
+            }
+            String textresult = activity;
+            //TextView placeText = (TextView)findViewById(R.id.textresult);
+            //placeText.setText(textresult);
+        }
+    }
+    public int randomNumber() {
+        int randomInt = new Random().nextInt(4) + 2;
+        return randomInt;
+    }
+
+    public List randomActions(int amount){
+        int i = 0;
+        List<String> myList = new ArrayList<String>();
+        myList.add("Flat");
+        myList.add("Right");
+        myList.add("Middle");
+        myList.add("Left");
+        myList.add("Down");
+        List picklockactivity = new ArrayList();
+        int lastrand = 300;
+        while(i < amount){
+
+            int rand = new Random().nextInt(myList.size());
+            if(lastrand == rand){
+                rand = (rand * rand * rand + 3) % myList.size();
+            }
+            lastrand = rand;
+            String activitiy = myList.get(rand);
+
+            picklockactivity.add(activitiy);
+            i = i + 1;
+        }
+        return picklockactivity;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
